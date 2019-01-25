cff21f5e3e2952515d85c422b6552acc4b3df62d
Jared Reando <jaredreando@gmail.com>
2019-01-25 14:40:50 -0800

    Add a new meal train component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
 import { NavFooterComponent } from './nav-footer/nav-footer.component';
 import { NavBodyComponent } from './nav-body/nav-body.component';
+import { NewTrainComponent } from './new-train/new-train.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavHeaderComponent,
     NavFooterComponent,
-    NavBodyComponent
+    NavBodyComponent,
+    NewTrainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new-train/new-train.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-train',
+  templateUrl: './new-train.component.html',
+  styleUrls: ['./new-train.component.css']
+})
+export class NewTrainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
