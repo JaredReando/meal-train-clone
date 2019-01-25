6c50108f73da99f81ed25d8b784b138c821c0cd6
Jared Reando <jaredreando@gmail.com>
2019-01-25 12:42:03 -0800

    Create event review area on main page for variable and function testing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Event } from './models/event.model';
 })
 export class AppComponent {
   title = 'meal-train';
+
+  allEvents: Event[] = [
+    new Event('Jared Reando', 'jaredreando@gmail.com', '13277 NE Eugene St.', 'Portland', 'OR', '97230', '503-863-4554', ['January 29, 2019', 'January 30, 2019', 'February 2, 2019'], '2', '1', 'Please delivery between 4 and 6pm if possible', 'We eat mostly simple meats and veggies.', 'Chicken and rice.', 'Just rice (no chicken)', 'Can\'t watermelons without cutting them up first.'),
+    new Event('Jack White', 'jackwhite@gmail.com', '13277 NE Eugene St.', 'Portland', 'OR', '97230', '503-863-4554', ['January 29, 2019', 'January 30, 2019', 'February 2, 2019'], '2', '1', 'Please delivery between 4 and 6pm if possible', 'We eat mostly simple meats and veggies.', 'Chicken and rice.', 'Just rice (no chicken)', 'Can\'t watermelons without cutting them up first.')
+  ]
+
 }
