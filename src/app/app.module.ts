7fe25242095b0b33ddf8bab37df7df9dfbc5c24e
Jared Reando <jaredreando@gmail.com>
2019-01-25 09:12:39 -0800

    Add site navigation header and footer components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { NavFooterComponent } from './nav-footer/nav-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent,
+    NavFooterComponent
   ],
   imports: [
     BrowserModule,
