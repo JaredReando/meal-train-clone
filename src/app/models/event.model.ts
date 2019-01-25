2ccaff22121b875ce82cddb033ae4f0b4776111d
Jared Reando <jaredreando@gmail.com>
2019-01-25 10:41:46 -0800

    Link components and Event model to app.component.ts file.

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -1,8 +1,22 @@
 export class Event {
+  public dateCreated = new Date();
 
-  constructor() {}
-
-
-
+  constructor(
+    public name: string,
+    public email: string,
+    public address: string,
+    public city: string,
+    public state: string,
+    public zip: string,
+    public phone: string,
+    public dates: any[],
+    public numAdults: string,
+    public numChildren: string,
+    public deliveryTimePreference: string,
+    public specialInstructions: string,
+    public favoriteMeals: string,
+    public leastFavoriteMeals: string,
+    public foodRestrictions: string
+  ) { }
 
 }
