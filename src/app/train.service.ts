6c4290b437faf0c5b6cb5509d86e36e0143c6ad1
Jared Reando <jaredreando@gmail.com>
2019-02-01 08:27:40 -0800

    Add Firebase DB connection to Train service.

--- src/app/train.service.ts
+++ src/app/train.service.ts
@@ -2,21 +2,27 @@ import { Injectable } from '@angular/core';
 import { Train } from './models/train.model';
 import { allTrains } from './mock-trains'
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TrainService {
+  allTrains: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.allTrains = database.list('trains')
+  }
 
   getTrains() {
-    return allTrains;
+    return this.allTrains;
   }
+
   getTrainById(trainId: any){
-    for (let i = 0; i < allTrains.length; i++) {
-      if(allTrains[i].name === trainId) {
-        return allTrains[i];
-      }
-    }
+    // for (let i = 0; i < allTrains.length; i++) {
+    //   if(allTrains[i].name === trainId) {
+    //     return allTrains[i];
+    //   }
+    // }
   }
 }
